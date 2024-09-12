4c4010b33ce728874d6d982369f2fe56a61ffa9b
Nathan <14392240@qq.com>
2024-09-12 22:12:44 +0800

    init

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuickStart' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for QuickStart
+  pod 'JuggleIM', '1.7.0'
+
+end
---
+++ QuickStart/AppDelegate.swift
@@ -0,0 +1,70 @@
+//
+//  AppDelegate.swift
+//  QuickStart
+//
+//  Created by Nathan on 2024/5/14.
+//
+
+import UIKit
+import JuggleIM
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        JIM.shared().setServer(["https://nav.juggleim.com"])
+        let appKey = "nsw3sue72begyv7y"
+        JIM.shared().setConsoleLogLevel(.verbose)
+        JIM.shared().initWithAppKey(appKey)
+        JIM.shared().messageManager.registerContentType(GroupNotifyMessage.self)
+        SBULog.logType = LogType.error.rawValue | LogType.warning.rawValue | LogType.info.rawValue
+        
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            case .notDetermined:
+                UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { granted, error in
+                    if granted {
+                        DispatchQueue.main.async {
+                            UIApplication.shared.registerForRemoteNotifications()
+                        }
+                    }
+                }
+            default:
+                break
+            }
+        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        JIM.shared().connectionManager.registerDeviceToken(deviceToken)
+        SBULog.info("token did register")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: any Error) {
+        SBULog.info("fail to register token, error is \(error.localizedDescription)")
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
